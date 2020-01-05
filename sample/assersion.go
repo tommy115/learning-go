705044f320a2e65ec305bc77e73edb761cef1e09
tommy115 <tommynecessary@gmail.com>
2020-01-06 00:08:28 +0900

    バグ修正

--- sample/assersion.go
+++ sample/assersion.go
@@ -10,8 +10,8 @@ func Anything(a interface{}) {
 
 func Anything2(x interface{}) {
 	i := x.(int)
-	fmt.Println("changed to int %v", i)
+	fmt.Printf("changed to int %v", i)
 	f := x.(float64)
-	fmt.Println("changed to float64 %v", f)
+	fmt.Printf("changed to float64 %v", f)
 
 }
