f2d5508174519f52fb8d0c4c9b973828f38cc41b
tommy115 <tommynecessary@gmail.com>
2020-01-06 00:08:51 +0900

    Printf様々なパターン

--- sample/formatter.go
+++ sample/formatter.go
@@ -6,6 +6,21 @@ import (
 
 // NumberSample 数値を表示するサンプル
 func NumberSample() {
-	fmt.Println("数値=%d\n", 5)
-	fmt.Println("10進数=%d 2進数=%b 8進数=%o 16進数=%x\n", 17, 17, 17, 17)
+	fmt.Printf("数値=%d\n", 5)
+	// 表示書式いろいろ
+	fmt.Printf("10進数=%d 2進数=%b 8進数=%o 16進数=%x\n", 17, 17, 17, 17)
+	// パラメータが足りないと、、、
+	fmt.Printf("%d年%d月%d日\n", 2020, 1)
+	// 過剰なパラメータの場合、、、、 lintで分かるが、、
+	fmt.Printf("%d年%d月%d日\n", 2020, 1, 5, 30)
+	fmt.Printf("\n")
+
+	// %v を試す
+	fmt.Printf("数値=%v 文字列=%v 配列=%v\n", 5, "Golang", [...]int{1, 2, 3})
+
+	// %#v を試す
+	fmt.Printf("数値=%#v 文字列=%#v 配列=%#v\n", 5, "Golang", [...]int{1, 2, 3})
+
+	// %Tは、、、
+	fmt.Printf("数値=%T 文字列=%T 配列=%T\n", 5, "Golang", [...]int{1, 2, 3})
 }
